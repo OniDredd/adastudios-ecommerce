5ac4a7eac7bd0a3d732850583f6a08629e81938b
Joshua Marsden <105263227+OniDredd@users.noreply.github.com>
2025-02-05 22:14:20 +1300

    Banner text smaller.

--- src/components/Homepage/Banner.tsx
+++ src/components/Homepage/Banner.tsx
@@ -10,7 +10,7 @@ export default function SaleBanner() {
     return (
       <section className="w-full h-screen flex flex-col justify-end items-center bg-[url('/HeroBackground.png')] bg-cover bg-center md:bg-top text-center pb-16 border-main-maroon border-b-[1px]">
         <FadeIn className="space-y-3">
-          <h1 className={`${inter.className} text-lg lg:text-3xl font-regular text-main-maroon`}>
+          <h1 className={`${inter.className} text-md lg:text-2xl font-regular text-main-maroon`}>
             New Year, New Look, Same glassware you love.
           </h1>
         </FadeIn>
